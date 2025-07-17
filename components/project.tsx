e64bd97fc3a19c6e54f7ba902ef230825d33d025
eakelvin <akabakelvin@gmail.com>
2025-07-17 18:05:45 +0200

    init project data

--- components/project.tsx
+++ components/project.tsx
@@ -12,6 +12,7 @@ import {
 import { Progress } from "@/components/ui/progress";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Link from 'next/link'
+import data from '@/utils/data';
 
 export const Project = () => {
     const [api, setApi] = useState<CarouselApi>();
@@ -34,50 +35,45 @@ export const Project = () => {
     }, [api]);
 
     return (
-        <div>
-            {/* <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-                {data.map((work) => (
-                    <Card key={work.id} {...work} />
-                ))}
-            </div> */}
+        <div className="py-4 px-10">
+            {data.map((work) => (
+                <div key={work.id} className="mb-10">
+                    <Carousel setApi={setApi} className="w-full max-w-full mx-auto">
+                        <CarouselContent>
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem key={index}>
+                                    <Card className='rounded-none'>
+                                        <CardContent className="flex aspect-video items-center justify-center p-6">
+                                            <span className="text-4xl font-semibold">{index + 1}</span>
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious className="top-[calc(100%+0.5rem)] translate-y-0 left-0" />
+                        <CarouselNext className="top-[calc(100%+0.5rem)] translate-y-0 left-2 translate-x-full" />
+                    </Carousel>
+                    <Progress value={progress} className="mt-4 w-24 ml-auto" />
 
-            <div className="py-4 px-10">
-                <Carousel setApi={setApi} className="w-full max-w-full mx-auto">
-                    <CarouselContent>
-                        {Array.from({ length: 5 }).map((_, index) => (
-                            <CarouselItem key={index}>
-                                <Card className='rounded-none'>
-                                    <CardContent className="flex aspect-video items-center justify-center p-6">
-                                        <span className="text-4xl font-semibold">{index + 1}</span>
-                                    </CardContent>
-                                </Card>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    <CarouselPrevious className="top-[calc(100%+0.5rem)] translate-y-0 left-0" />
-                    <CarouselNext className="top-[calc(100%+0.5rem)] translate-y-0 left-2 translate-x-full" />
-                </Carousel>
-                <Progress value={progress} className="mt-4 w-24 ml-auto" />
-
-                <div className="text-white mx-auto space-y-4 mt-8">
-                    <h2 className='text-xl font-bold'>
-                        Building Once UI, a customizable Design System
-                    </h2>
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <p className='text-sm'>
-                        Development of a flexible and highly customizable design system using Next.js for front-end and Figma for design collaboration.
-                    </p>
-                    <div className='mt-1'>
-                        <Link href="#" className='text-cyan-500 hover:underline'>
-                            View Project
-                        </Link>
+                    <div className="text-white mx-auto space-y-4 mt-8">
+                        <h2 className='text-xl font-bold'>
+                            {work.title}
+                        </h2>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <p className='text-sm'>
+                            {work.description}
+                        </p>
+                        <div className='mt-1'>
+                            <Link href={work.website} className='text-cyan-500 hover:underline'>
+                                View Project
+                            </Link>
+                        </div>
                     </div>
-
                 </div>
-            </div>
+            ))}
         </div>
     )
 }
